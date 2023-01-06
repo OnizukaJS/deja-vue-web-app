67f4e15b50a1c298565268c0b6cb32ed2d4bb8e6
Jean-Baptiste Castillo <jeanbaptiste.castillo@isolutions.ch>
2023-01-06 23:25:09 +0100

    Created basic home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopRatedMovies from "./TopRatedMoviesPage";
+import PopularMovies from "./PopularMoviesPage";
+
+const HomePage = () => {
+  return (
+    <>
+      <p>Home Page</p>
+    </>
+  );
+};
+
+export default HomePage;
