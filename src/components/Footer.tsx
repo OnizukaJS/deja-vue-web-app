c373a1e6ebb8848a1a6b1d10aae5d94de9f747ce
Jean-Baptiste Castillo <jeanbaptiste.castillo@isolutions.ch>
2023-01-09 09:53:46 +0100

    Updated footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import tmdbLogo from "../images/tmdb_logo.svg";
+import { createStyles, makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    containerHeader: {
+      display: "flex",
+      marginTop: "auto",
+      justifyContent: "space-between",
+      alignItems: "center",
+      borderTop: "solid 3px",
+      borderTopColor: "#90cea1",
+      height: 100,
+    },
+  })
+);
 
 const Footer = () => {
+  const classes = useStyles();
+
   return (
-    <footer style={{ display: "flex", marginTop: "auto" }}>
+    <footer className={classes.containerHeader}>
       <img src={tmdbLogo} alt="TMDB_Logo" style={{ width: 100 }} />
     </footer>
   );
