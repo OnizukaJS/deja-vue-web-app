f07dbce41ca8d3ca3b0c546f3cdf4984f25128c8
Jean-Baptiste Castillo <jeanbaptiste.castillo@isolutions.ch>
2023-01-15 19:00:12 +0100

    Finished serie recommendations

--- src/components/SerieRecommendations.tsx
+++ src/components/SerieRecommendations.tsx
@@ -1,12 +1,62 @@
 import React from "react";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { createStyles, makeStyles } from "@mui/styles";
 import SerieRecommendationModel from "../models/SerieRecommendationModel";
+import { useNavigate } from "react-router-dom";
+import routes from "../constants/routes";
+import RateBadge from "./RateBadge";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    containerSerieRecommendation: {
+      minHeight: 141,
+      minWidth: 250,
+      margin: "6px",
+    },
+    image: {
+      height: 141,
+      width: 250,
+    },
+  })
+);
 
 interface SerieRecommendationsProps {
   serie: SerieRecommendationModel;
 }
 
 const SerieRecommendations = ({ serie }: SerieRecommendationsProps) => {
-  return <div>{serie.name}</div>;
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  return (
+    <Card className={classes.containerSerieRecommendation}>
+      <CardActionArea
+        onClick={() => navigate(routes.serieDetails(serie.id.toString()))}
+        sx={{ height: "100%" }}
+      >
+        <Box sx={{ height: "100%" }}>
+          <CardMedia
+            component="img"
+            image={`https://www.themoviedb.org/t/p/original${serie.backdrop_path}`}
+            alt={serie.name}
+            className={classes.image}
+          />
+          <CardContent>
+            <Typography>{serie.name}</Typography>
+            <Typography>{serie.first_air_date}</Typography>
+            <RateBadge rate={Number(serie.vote_average.toFixed(1))} />
+          </CardContent>
+        </Box>
+      </CardActionArea>
+    </Card>
+  );
 };
 
 export default SerieRecommendations;
